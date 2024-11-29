c61102cbf502fa572f5214bd774d1f62881b47ca
HETNOOR <daffixipop@gmail.com>
2024-11-29 20:13:32 +0700

    Initial Commit

---
+++ Storage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storage
+//
+//  Created by Максим Герасимов on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
